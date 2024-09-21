fc703df2bbd450a7daec2a6c85cb77ae0b19dd32
Zigalow <s224302@dtu.dk>
2024-09-21 22:16:16 +0200

    Changed name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/devops_week1_test/'
+  base: '/devops_week1_vite/'
 })
